06d2d6655fdfc194930dd8bf30bbb7fc5a90a079
Maarij07 <syedmuhammadmaarij@gmail.com>
2025-08-26 11:26:38 +0500

    modal design changed

--- check_retention.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from config.settings import Settings
-
-settings = Settings()
-retention_hours = settings.get("DATA_RETENTION_HOURS")
-print(f"DATA_RETENTION_HOURS: {retention_hours}")
-print(f"This means products older than {retention_hours} hours will be automatically removed.")
